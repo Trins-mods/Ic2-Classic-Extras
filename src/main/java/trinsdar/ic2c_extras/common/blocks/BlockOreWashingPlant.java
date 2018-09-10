fe88647bc3c3181c4f671606be32c1d65d8b93f5
Razzokk <alexzelger48@gmail.com>
2018-09-10 18:34:38 +0200

    Added IC2C Dependecy, Added Registries and Base Files for Blocks and Items

---
+++ src/main/java/trinsdar/ic2c_extras/common/blocks/BlockOreWashingPlant.java
@@ -0,0 +1,11 @@
+package trinsdar.ic2c_extras.common.blocks;
+
+import net.minecraft.block.material.Material;
+
+public class BlockOreWashingPlant extends BlockBase
+{
+    public BlockOreWashingPlant(Material materialIn, String blockName)
+    {
+        super(materialIn, blockName);
+    }
+}
--- src/main/java/trinsdar/ic2c_extras/common/items/ItemCrushedOre.java
+++ src/main/java/trinsdar/ic2c_extras/common/items/ItemCrushedOre.java
@@ -1,9 +1,20 @@
 package trinsdar.ic2c_extras.common.items;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
 public class ItemCrushedOre extends ItemBase
 {
     public ItemCrushedOre(String itemName)
     {
         super(itemName);
+        this.hasSubtypes = true;
+    }
+
+    @Override
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items)
+    {
+        super.getSubItems(tab, items);
     }
 }
