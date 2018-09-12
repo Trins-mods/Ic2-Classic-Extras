4c373a4a147fc383eda76d31f8a0617e5e875324
Trinsdar <westbound1715@gmail.com>
2018-09-12 12:47:38 -0400

    Added class for gui locations.

---
+++ src/main/java/trinsdar/ic2c_extras/common/util/Ic2cExtrasGuiLocations.java
@@ -0,0 +1,11 @@
+package trinsdar.ic2c_extras.common.util;
+
+import net.minecraft.util.ResourceLocation;
+
+public class Ic2cExtrasGuiLocations {
+    public static ResourceLocation oreWashingPlant = new ResourceLocation("ic2c_extras", "textures/guiSprites/GUIOreWashingPlant");
+
+    public Ic2cExtrasGuiLocations(){
+    }
+
+}
