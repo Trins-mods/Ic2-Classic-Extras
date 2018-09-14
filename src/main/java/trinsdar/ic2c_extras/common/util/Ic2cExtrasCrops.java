1e0fe41519b7fb857744dfea2031e943aff48a3d
Trinsdar <westbound1715@gmail.com>
2018-09-14 15:48:32 -0400

    not yet finished registering of crops.

---
+++ src/main/java/trinsdar/ic2c_extras/common/util/Ic2cExtrasCrops.java
@@ -0,0 +1,19 @@
+package trinsdar.ic2c_extras.common.util;
+
+import ic2.api.crops.CropCard;
+import ic2.api.crops.Crops;
+import ic2.core.block.crop.Ic2Crops;
+import ic2.core.block.crop.crops.CropWheat;
+import ic2.core.platform.registry.Ic2Items;
+import trinsdar.ic2c_extras.common.blocks.CropGalena;
+
+public class Ic2cExtrasCrops extends Ic2Crops {
+
+//    public static Ic2cExtrasCrops instance;
+//
+//
+//
+//    public static CropRegisterEvent cropGalena = new CropGalena();
+//
+
+}
