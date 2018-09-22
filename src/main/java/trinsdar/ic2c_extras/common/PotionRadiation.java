f89a22fc8e093c630b26f7592a5cccd6a534f40f
Trinsdar <westbound1715@gmail.com>
2018-09-22 00:17:35 -0400

    Added Radiation effect, though it doesn't want to register yet.
    Also turns out Ic2 classic already has the radiation effect.

---
+++ src/main/java/trinsdar/ic2c_extras/common/PotionRadiation.java
@@ -0,0 +1,72 @@
+package trinsdar.ic2c_extras.common;
+
+import ic2.api.info.Info;
+import ic2.core.entity.IC2DamageSource;
+import ic2.core.entity.IC2Potion;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import trinsdar.ic2c_extras.Ic2cExtras;
+import trinsdar.ic2c_extras.common.util.Icons;
+
+import java.util.List;
+
+public class PotionRadiation extends Potion
+{
+    public static PotionRadiation radiation;
+
+    private static final ResourceLocation ICON = new ResourceLocation(Ic2cExtras.MODID, "textures/guisprites/radiation_icon");
+    private final java.util.List<ItemStack> effectedItems;
+
+    public PotionRadiation(boolean isBadEffect, int liquidColour, ItemStack... effectedItems){
+        super(isBadEffect, liquidColour);
+        this.setPotionName("potion." + Ic2cExtras.MODID + ":radiation");
+        this.effectedItems = java.util.Arrays.asList(effectedItems);
+
+    }
+
+    public void performEffect(EntityLivingBase entity, int amplifier) {
+        if (this == radiation) {
+            entity.attackEntityFrom(IC2DamageSource.radiation, (float)(amplifier + 1));
+        }
+
+    }
+
+    @Override
+    public boolean isReady(int p_76397_1_, int p_76397_2_){
+        // Copied from the vanilla wither effect. It does the timing stuff. 25 is the number of ticks between hits at
+        // amplifier 0
+        int k = 40 >> p_76397_2_;
+        return k > 0 ? p_76397_1_ % k == 0 : true;
+    }
+
+//    @Override
+//    @SideOnly(Side.CLIENT)
+//    public void renderInventoryEffect(int x, int y, PotionEffect effect, net.minecraft.client.Minecraft mc){
+//        mc.renderEngine.bindTexture(ICON);
+//        Icons.drawTexturedRect(x + 6, y + 7, 0, 0, 18, 18, 18, 18);
+//    }
+//
+//    @Override
+//    @SideOnly(Side.CLIENT)
+//    public void renderHUDEffect(int x, int y, PotionEffect effect, net.minecraft.client.Minecraft mc, float alpha){
+//        mc.renderEngine.bindTexture(ICON);
+//        Icons.drawTexturedRect(x + 3, y + 3, 0, 0, 18, 18, 18, 18);
+//    }
+
+    public static void init()
+    {
+        Info.POTION_RADIATION = radiation = new PotionRadiation(true, 5149489);
+        Info.POTION_RADIATION.setRegistryName(new ResourceLocation("ic2c_extras", "radiation"));
+        ForgeRegistries.POTIONS.register(radiation);
+        radiation.setPotionName("potion.radiation.name");
+        radiation.setIconIndex(6, 0);
+        radiation.setEffectiveness(0.25D);
+    }
+
+}
--- src/main/java/trinsdar/ic2c_extras/common/util/Icons.java
+++ src/main/java/trinsdar/ic2c_extras/common/util/Icons.java
@@ -2,6 +2,7 @@ package trinsdar.ic2c_extras.common.util;
 
 import ic2.core.platform.textures.Ic2Icons;
 import ic2.core.platform.textures.Sprites;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -20,5 +21,29 @@ public class Icons
         addTextureEntry(new Sprites.TextureEntry("ic2c_extras_items", 0, 2, 11, 3));
         addTextureEntry(new Sprites.TextureEntry("ic2c_extras_items", 0, 3, 9, 4));
         addTextureEntry(new Sprites.TextureEntry("ic2c_extras_items", 0, 4, 9, 5));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void drawTexturedRect(int x, int y, int u, int v, int width, int height, int textureWidth, int textureHeight)
+    {
+
+        float f = 1F / (float)textureWidth;
+        float f1 = 1F / (float)textureHeight;
+        BufferBuilder buffer = net.minecraft.client.renderer.Tessellator.getInstance()
+                .getBuffer();
+        buffer.begin(org.lwjgl.opengl.GL11.GL_QUADS,
+                net.minecraft.client.renderer.vertex.DefaultVertexFormats.POSITION_TEX);
+        buffer.pos((double)(x), (double)(y + height), 0)
+                .tex((double)((float)(u) * f), (double)((float)(v + height) * f1)).endVertex();
+        buffer.pos((double)(x + width), (double)(y + height), 0)
+                .tex((double)((float)(u + width) * f), (double)((float)(v + height) * f1)).endVertex();
+        buffer.pos((double)(x + width), (double)(y), 0).tex((double)((float)(u + width) * f), (double)((float)(v) * f1))
+                .endVertex();
+        buffer.pos((double)(x), (double)(y), 0).tex((double)((float)(u) * f), (double)((float)(v) * f1)).endVertex();
+        net.minecraft.client.renderer.Tessellator.getInstance().draw();
+    }
+    @SideOnly(Side.CLIENT)
+    public static void drawTexturedRect(int x, int y, int width, int height){
+        drawTexturedRect(x, y, 0, 0, width, height, width, height);
     }
 }
