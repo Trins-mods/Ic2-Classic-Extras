a41ebcdd16a60d62ad9308f448b1344e06b45c02
Trinsdar <westbound1715@gmail.com>
2018-09-23 22:00:00 -0400

    added radiation effect activating that won't work.

--- src/main/java/trinsdar/ic2c_extras/common/Radiation.java
+++ src/main/java/trinsdar/ic2c_extras/common/Radiation.java
@@ -1,14 +1,19 @@
 package trinsdar.ic2c_extras.common;
 
+import ic2.api.classic.item.IHazmatSuit;
 import ic2.api.info.Info;
+import ic2.core.IC2;
 import ic2.core.entity.IC2DamageSource;
 import ic2.core.entity.IC2Potion;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
@@ -24,12 +29,27 @@ import static ic2.core.item.armor.standart.ItemHazmatArmor.isFullHazmatSuit;
 public class Radiation
 {
 
-//    @SubscribeEvent
-//    public void onPlayerTick(PlayerTickEvent event){
-//        EntityPlayer player = event.getPlayer();
-//        if (!player.isCreative())
-//        {
-//            if((!isFullHazmatSuit(player) )){}
+    @SubscribeEvent
+    public void onPlayerTick(PlayerTickEvent event){
+        EntityPlayer player = event.player;
+        ItemStack stack = new ItemStack(Items.IRON_INGOT);
+        if (!player.isCreative())
+        {
+            if((!isFullHazmatSuit(player)) && player.inventory.hasItemStack(stack))
+            {
+                player.addPotionEffect(new PotionEffect(IC2Potion.radiation, 90, 0));
+            }
+        }
+    }
+
+
+//    public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
+//        boolean server = IC2.platform.isSimulating();
+//        if (server) {
+//            PotionEffect radiation = player.getActivePotionEffect(IC2Potion.radiation);
+//            if (radiation != null && isFullHazmatSuit(player)) {
+//                IC2.platform.removePotion(player, IC2Potion.radiation);
+//            }
 //        }
 //    }
 
