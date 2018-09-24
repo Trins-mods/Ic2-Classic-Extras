adf3dcd630028719c4cfea1487ae83290b00425d
Trinsdar <westbound1715@gmail.com>
2018-09-23 23:48:48 -0400

    started work towards having radiation given on certian items.

--- src/main/java/trinsdar/ic2c_extras/common/Radiation.java
+++ src/main/java/trinsdar/ic2c_extras/common/Radiation.java
@@ -5,6 +5,7 @@ import ic2.api.info.Info;
 import ic2.core.IC2;
 import ic2.core.entity.IC2DamageSource;
 import ic2.core.entity.IC2Potion;
+import ic2.core.platform.registry.Ic2Items;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
@@ -22,9 +23,13 @@ import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import trinsdar.ic2c_extras.Ic2cExtras;
+import trinsdar.ic2c_extras.common.items.RegistryItem;
 import trinsdar.ic2c_extras.common.util.Icons;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static ic2.core.item.armor.standart.ItemHazmatArmor.isFullHazmatSuit;
 
@@ -34,7 +39,15 @@ public class Radiation
     @SubscribeEvent
     public void onPlayerTick(PlayerTickEvent event){
         EntityPlayer player = event.player;
-        ItemStack stack = new ItemStack(Items.IRON_INGOT);
+        Set<ItemStack> radiation = new HashSet<>();
+        radiation.add(new ItemStack(RegistryItem.itemMiscs, 1, 4));
+        radiation.add(new ItemStack(RegistryItem.itemMiscs, 1, 5));
+        radiation.add(new ItemStack(RegistryItem.itemMiscs, 1, 5));
+        radiation.add(new ItemStack(RegistryItem.tinyDustTypes, 1, 5));
+        radiation.add(new ItemStack(RegistryItem.tinyDustTypes, 1, 9));
+        radiation.add(new ItemStack(RegistryItem.tinyDustTypes, 1, 10));
+        radiation.add(Ic2Items.uraniumOre);
+        ItemStack stack = (Ic2Items.uraniumOre);
         if (event.phase == TickEvent.Phase.END) {
             if (!player.isCreative()) {
                 if ((!isFullHazmatSuit(player)) && player.inventory.hasItemStack(stack)) {
