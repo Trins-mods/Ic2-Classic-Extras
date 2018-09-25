5a419dd38323923854891934dbb7fe5714fee353
Trinsdar <westbound1715@gmail.com>
2018-09-25 19:50:24 -0400

    Steel fully implemented except for the texture.

--- src/main/java/trinsdar/ic2c_extras/common/blocks/BlockSteel.java
+++ src/main/java/trinsdar/ic2c_extras/common/blocks/BlockSteel.java
@@ -3,8 +3,11 @@ package trinsdar.ic2c_extras.common.blocks;
 import ic2.core.block.resources.BlockMetal;
 import ic2.core.platform.textures.Ic2Icons;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import trinsdar.ic2c_extras.Ic2cExtras;
 
 import java.util.Arrays;
@@ -15,6 +18,8 @@ public class BlockSteel extends Block {
     public BlockSteel(String blockName){
         super(Material.IRON);
         this.setHardness(4.0F);
+        this.setResistance(10.0F);
+        this.setSoundType(SoundType.METAL);
         this.setHarvestLevel("pickaxe", 2);
         this.setCreativeTab(Ic2cExtras.creativeTab);
         this.setRegistryName(Ic2cExtras.MODID, blockName);
@@ -23,6 +28,10 @@ public class BlockSteel extends Block {
 
     public List<Integer> getValidMetas() {
         return Arrays.asList(0);
+    }
+    public boolean isBeaconBase(IBlockAccess worldObj, BlockPos pos, BlockPos beacon) {
+
+        return true;
     }
 
     public TextureAtlasSprite[] getIconSheet(int meta)
