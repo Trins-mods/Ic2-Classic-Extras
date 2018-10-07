fe464f1a752d8e1303ab787a68c1840f5448000c
Trinsdar <westbound1715@gmail.com>
2018-10-06 22:43:54 -0400

    added basics for adding entries to ic2 config

--- src/main/java/trinsdar/ic2c_extras/common/Config.java
+++ src/main/java/trinsdar/ic2c_extras/common/Config.java
@@ -1,19 +1,22 @@
 package trinsdar.ic2c_extras.common;
 import ic2.core.IC2;
 import ic2.api.classic.addon.IC2Plugin;
+import ic2.core.platform.config.ConfigEntry;
+import ic2.core.platform.config.IC2Config;
 import trinsdar.ic2c_extras.common.proxy.CommonProxy;
 import net.minecraftforge.common.config.Configuration;
 import trinsdar.ic2c_extras.Ic2cExtras;
 import org.apache.logging.log4j.Level;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Config {
     private static final String CATEGORY_GENERAL = "general";
     private static final String CATEGORY_RADIATION = "radiation";
 
     // This values below you can access elsewhere in your mod:
-    public static boolean isThisAGoodTutorial = true;
     public static boolean isRadiationTurnedOn = true;
-    public static String yourRealName = "Steve";
 
     // Call this from CommonProxy.preInit(). It will create our config if it doesn't
     // exist yet and read the values if it does exist.
@@ -35,13 +38,23 @@ public class Config {
     private static void initGeneralConfig(Configuration cfg) {
         cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration");
         // cfg.getBoolean() will get the value in the config if it is already specified there. If not it will create the value.
-        isThisAGoodTutorial = cfg.getBoolean("goodTutorial", CATEGORY_GENERAL, isThisAGoodTutorial, "Set to false if you don't like this tutorial");
-        yourRealName = cfg.getString("realName", CATEGORY_GENERAL, yourRealName, "Set your real name here");
     }
 
     private static void initRadiationConfig(Configuration cfg) {
         cfg.addCustomCategoryComment(CATEGORY_RADIATION, "Radiation configuration");
         isRadiationTurnedOn = cfg.getBoolean("radiationSwitch", CATEGORY_GENERAL, isRadiationTurnedOn, "Set to false if you don't want radiation on.");
 
+    }
+    List<ConfigEntry> defaults = new ArrayList();
+
+    private void init(IC2Config config){
+        IC2Config.ConfigType bool = IC2Config.ConfigType.Boolean;
+        IC2Config.ConfigType intg = IC2Config.ConfigType.Integer;
+        IC2Config.ConfigType flo = IC2Config.ConfigType.Float;
+        IC2Config.ConfigType txt = IC2Config.ConfigType.String;
+        this.add((new ConfigEntry(bool, "radiation", "enableItemRadiation", "Enable certain items giving radiation", "ItemRadiation", true)).setGameRestart().setServerSync());
+    }
+    private void add(ConfigEntry par1) {
+        this.defaults.add(par1);
     }
 }
