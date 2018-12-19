f703e546b975ee14d4d7ea64c14b3fcf9e194e59
Trinsdar <westbound1715@gmail.com>
2018-12-19 14:06:13 -0500

    resized images and added block textures

--- src/main/java/trinsdar/ic2c_extras/util/Icons.java
+++ src/main/java/trinsdar/ic2c_extras/util/Icons.java
@@ -12,7 +12,7 @@ public class Icons
     @SideOnly(Side.CLIENT)
     public static void loadSprites()
     {
-        addSprite(new Sprites.SpriteData("ic2c_extras_blocks", "ic2c_extras:textures/sprites/blocks.png", new Sprites.SpriteInfo(4, 4)));
+        addSprite(new Sprites.SpriteData("ic2c_extras_blocks", "ic2c_extras:textures/sprites/blocks.png", new Sprites.SpriteInfo(2, 2)));
         addSprite(new Sprites.SpriteData("thermalcentrifuge", "ic2c_extras:textures/sprites/thermalcentrifuge.png", new Sprites.SpriteInfo(1, 12)));
         addSprite(new Sprites.SpriteData("orewashingplant", "ic2c_extras:textures/sprites/orewashingplant.png", new Sprites.SpriteInfo(1, 12)));
         addSprite(new Sprites.SpriteData("metalpress", "ic2c_extras:textures/sprites/metalpress.png", new Sprites.SpriteInfo(1, 12)));
@@ -21,8 +21,8 @@ public class Icons
         addSprite(new Sprites.SpriteData("solidfuelfirebox", "ic2c_extras:textures/sprites/solidfuelfirebox.png", new Sprites.SpriteInfo(1, 12)));
         addSprite(new Sprites.SpriteData("liquidfuelfirebox", "ic2c_extras:textures/sprites/liquidfuelfirebox.png", new Sprites.SpriteInfo(1, 12)));
         addSprite(new Sprites.SpriteData("electricheater", "ic2c_extras:textures/sprites/electricheater.png", new Sprites.SpriteInfo(1, 12)));
-        addSprite(new Sprites.SpriteData("ic2c_extras_items", "ic2c_extras:textures/sprites/items.png", new Sprites.SpriteInfo(16, 16)));
-        addTextureEntry(new Sprites.TextureEntry("ic2c_extras_blocks", 0, 0, 4, 4));
+        addSprite(new Sprites.SpriteData("ic2c_extras_items", "ic2c_extras:textures/sprites/items.png", new Sprites.SpriteInfo(16, 5)));
+        addTextureEntry(new Sprites.TextureEntry("ic2c_extras_blocks", 0, 0, 2, 2));
         addTextureEntry(new Sprites.TextureEntry("thermalcentrifuge", 0, 0, 1, 12));
         addTextureEntry(new Sprites.TextureEntry("orewashingplant", 0, 0, 1, 12));
         addTextureEntry(new Sprites.TextureEntry("metalpress", 0, 0, 1, 12));
@@ -31,6 +31,6 @@ public class Icons
         addTextureEntry(new Sprites.TextureEntry("solidfuelfirebox", 0, 0, 1, 12));
         addTextureEntry(new Sprites.TextureEntry("liquidfuelfirebox", 0, 0, 1, 12));
         addTextureEntry(new Sprites.TextureEntry("electricheater", 0, 0, 1, 12));
-        addTextureEntry(new Sprites.TextureEntry("ic2c_extras_items", 0, 0, 16, 16));
+        addTextureEntry(new Sprites.TextureEntry("ic2c_extras_items", 0, 0, 16, 5));
     }
 }
