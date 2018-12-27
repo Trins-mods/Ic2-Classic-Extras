a4fbc09fb4154487592813367af823c30ef0db6e
Trinsdar <westbound1715@gmail.com>
2018-12-27 14:16:29 -0500

    renamed dupe preventer

---
+++ src/main/java/trinsdar/ic2c_extras/IC2CExtrasDupePreventer.java
@@ -0,0 +1,7 @@
+package trinsdar.ic2c_extras;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "ic2c_extrasdupeprvnt", name = "Ic2cExtrasDupePreventer", version = IC2CExtras.VERSION)
+public class IC2CExtrasDupePreventer {
+}
