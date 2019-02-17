ccb51ab71459b27b860b0e1933b6d0da2698f5d3
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-02-17 18:44:00 -0500

    added ic2 classic dependency check

--- src/main/java/trinsdar/ic2c_extras/IC2CExtrasDupePreventer.java
+++ src/main/java/trinsdar/ic2c_extras/IC2CExtrasDupePreventer.java
@@ -2,6 +2,6 @@ package trinsdar.ic2c_extras;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "ic2c_extrasdupeprvnt", name = "Ic2cExtrasDupePreventer", version = IC2CExtras.VERSION)
+@Mod(modid = "ic2c_extrasdupeprvnt", name = "Ic2cExtrasDupePreventer", version = IC2CExtras.VERSION, dependencies = "required-after:ic2;required-after:ic2-classic-spmod")
 public class IC2CExtrasDupePreventer {
 }
