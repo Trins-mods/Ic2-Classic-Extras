2ab430019a44469418d5a37181d8321ce1a3cbd3
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-04-27 17:32:29 -0400

    added crafttweaker support for metal bender

---
+++ src/main/java/trinsdar/ic2c_extras/util/crafttweaker/MetalBenderSupport.java
@@ -0,0 +1,43 @@
+package trinsdar.ic2c_extras.util.crafttweaker;
+
+import crafttweaker.IAction;
+import crafttweaker.api.item.IIngredient;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import ic2.api.classic.recipe.ClassicRecipes;
+import ic2.api.recipe.IRecipeInput;
+import net.minecraft.item.ItemStack;
+import stanhebben.zenscript.annotations.ZenMethod;
+import trinsdar.ic2c_extras.blocks.tileentity.TileEntityMetalBender;
+
+import java.util.Locale;
+
+public class MetalBenderSupport {
+    @ZenMethod
+    public static void addRecipe(IItemStack output, IIngredient input, IItemStack press) {
+        CraftTweakerPlugin.apply(new MetalBenderRecipeAction(CraftTweakerPlugin.of(input), CraftTweakerMC.getItemStack(press), CraftTweakerMC.getItemStack(output)));
+    }
+
+    private static final class MetalBenderRecipeAction implements IAction {
+
+        private final IRecipeInput input;
+        private final ItemStack press;
+        private final ItemStack output;
+
+        MetalBenderRecipeAction(IRecipeInput input, ItemStack press, ItemStack output) {
+            this.input = input;
+            this.press = press;
+            this.output = output;
+        }
+
+        @Override
+        public void apply() {
+            TileEntityMetalBender.addRecipe(this.input, this.press, this.output);
+        }
+
+        @Override
+        public String describe() {
+            return String.format(Locale.ENGLISH, "Add Recipe[%s, %s -> %s] to %s", this.input, this.press, this.output, TileEntityMetalBender.metalBender);
+        }
+    }
+}
