28bd67e81e17410025224e0b55e46be2af8b7748
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-12-15 20:45:58 -0500

    fixed left hand model

--- src/main/java/trinsdar/ic2c_extras/items/ItemToolCrafting.java
+++ src/main/java/trinsdar/ic2c_extras/items/ItemToolCrafting.java
@@ -1,10 +1,12 @@
 package trinsdar.ic2c_extras.items;
 
 import ic2.core.platform.textures.Ic2Icons;
+import ic2.core.platform.textures.obj.ICustomItemCameraTransform;
 import ic2.core.platform.textures.obj.IStaticTexturedItem;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import trinsdar.ic2c_extras.IC2CExtras;
@@ -12,7 +14,7 @@ import trinsdar.ic2c_extras.IC2CExtras;
 import java.util.Arrays;
 import java.util.List;
 
-public class ItemToolCrafting extends Item implements IStaticTexturedItem {
+public class ItemToolCrafting extends Item implements IStaticTexturedItem, ICustomItemCameraTransform {
     int itemIndex;
     boolean enchant;
 
@@ -54,5 +56,15 @@ public class ItemToolCrafting extends Item implements IStaticTexturedItem {
     @SideOnly(Side.CLIENT)
     public TextureAtlasSprite getTexture(int meta) {
         return Ic2Icons.getTextures("ic2c_extras_tools")[this.itemIndex];
+    }
+
+    @Override
+    public boolean hasCustomTransform(int meta) {
+        return true;
+    }
+
+    @Override
+    public ResourceLocation getCustomTransform(int meta) {
+        return new ResourceLocation("minecraft:models/item/handheld");
     }
 }
