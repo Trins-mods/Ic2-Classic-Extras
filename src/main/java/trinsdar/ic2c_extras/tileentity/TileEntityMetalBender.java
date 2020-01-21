9c0aa120d547d85b954eb07f029f068878572bfc
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-01-20 23:36:56 -0500

    maybe fix #11

--- src/main/java/trinsdar/ic2c_extras/tileentity/TileEntityMetalBender.java
+++ src/main/java/trinsdar/ic2c_extras/tileentity/TileEntityMetalBender.java
@@ -55,6 +55,7 @@ public class TileEntityMetalBender extends TileEntityContainerInputBase {
         handler.registerDefaultSlotsForSide(RotationList.UP.invert(), slotOutput);
         handler.registerDefaultSlotsForSide(RotationList.DOWN.invert(), slotInput);
         handler.registerInputFilter(new ArrayFilter(CommonFilters.DischargeEU, new BasicItemFilter(Items.REDSTONE), new BasicItemFilter(Ic2Items.suBattery)), 2);
+        handler.registerInputFilter(filter, slotInput);
         handler.registerOutputFilter(CommonFilters.NotDischargeEU, 2);
         handler.registerSlotType(SlotType.Fuel, 2);
         handler.registerSlotType(SlotType.Input, slotInput);
