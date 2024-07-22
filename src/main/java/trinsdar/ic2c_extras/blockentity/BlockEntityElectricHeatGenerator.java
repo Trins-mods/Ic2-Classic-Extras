58fd634dcb3d968f81629fe7157d8b22125f093e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-07-21 21:18:20 -0400

    added code to electric heat generator tile for creating heat from eu

--- src/main/java/trinsdar/ic2c_extras/blockentity/BlockEntityElectricHeatGenerator.java
+++ src/main/java/trinsdar/ic2c_extras/blockentity/BlockEntityElectricHeatGenerator.java
@@ -3,6 +3,7 @@ package trinsdar.ic2c_extras.blockentity;
 import ic2.api.util.DirectionList;
 import ic2.core.block.base.cache.CapabilityCache;
 import ic2.core.block.base.cache.ICache;
+import ic2.core.block.base.features.ITickListener;
 import ic2.core.block.base.tiles.BaseElectricTileEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -14,12 +15,12 @@ import net.minecraftforge.fluids.capability.IFluidHandler;
 import tesseract.api.forge.TesseractCaps;
 import tesseract.api.heat.IHeatHandler;
 
-public class BlockEntityElectricHeatGenerator extends BaseElectricTileEntity implements IHeatTile {
+public class BlockEntityElectricHeatGenerator extends BaseElectricTileEntity implements IHeatTile, ITickListener {
     final Ic2cExtrasHeatHandler heatHandler;
     ICache<IHeatHandler> heatCache;
     public BlockEntityElectricHeatGenerator(BlockPos pos, BlockState state) {
         super(pos, state, 1, 32, 10000);
-        heatHandler = new Ic2cExtrasHeatHandler(0, 0, 32, this);
+        heatHandler = new Ic2cExtrasHeatHandler(128, 0, 32, this);
         heatCache = new CapabilityCache<>(this, DirectionList.ALL, TesseractCaps.HEAT_CAPABILITY);
         this.addCaches(heatCache);
         this.addCapability(TesseractCaps.HEAT_CAPABILITY, heatHandler);
@@ -48,5 +49,18 @@ public class BlockEntityElectricHeatGenerator extends BaseElectricTileEntity imp
     @Override
     public boolean canOutput(Direction direction) {
         return direction == this.getFacing();
+    }
+
+    @Override
+    public void onTick() {
+        if (getStoredEU() > 0 && heatHandler.getHeat() < heatHandler.getHeatCap()){
+            int min = Math.min(Math.min(10, getStoredEU()), heatHandler.getHeatCap() - heatHandler.getHeat());
+            this.useEnergy(min);
+            heatHandler.insertInternal(min, false);
+            if (!this.isActive()) setActive(true);
+        } else {
+            if (this.isActive()) setActive(false);
+        }
+        this.heatHandler.update(this.isActive());
     }
 }
